a9874869d32c466af113a5872b1bc3efb3496163
Sakura Dragon <anon@anon.com>
2020-05-13 11:23:27 +0100

    Added Argument to hello world

--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -6,7 +6,14 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length >0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Who are you?");
+            }
         }
     }
 }
