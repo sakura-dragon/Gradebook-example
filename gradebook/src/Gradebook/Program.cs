e5fb8a6363053987c5c3a9c2991d18157ff4184e
Sakura Dragon <anon@anon.com>
2020-06-02 09:44:13 +0100

    extract main while loop method to private class

--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -7,31 +7,36 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            Book mathsBook = new Book("Maths");
+            Book book = new Book("Maths");
+            book.GradeAdded += OnGradeAdded;
+            book.GradeAdded -= OnGradeAdded;// Removed the previous method.
+            book.GradeAdded += OnGradeAdded;// There is only one subscription now.
+
+            book.AddGrades(args);
+            EnterGrade(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"Average grade for {book.Name} is = {stats.Average:N2}");
+            Console.WriteLine($"Highest grade in {book.Name} is = {stats.High:N2}");
+            Console.WriteLine($"Lowest grade in {book.Name} is = {stats.Low:N2}");
+            Console.WriteLine($"The letter grade in {book.Name} is = {stats.Letter}");
+        }
+
+        private static bool EnterGrade(Book book)
+        {
             bool done = false;
-            mathsBook.GradeAdded += OnGradeAdded;
-            mathsBook.GradeAdded -= OnGradeAdded;// Removed the previous method.
-            mathsBook.GradeAdded += OnGradeAdded;// There is only one subscription now.
-
-            mathsBook.AddGrades(args);
-
-            while(!done)
+            while (!done)
             {
-                Console.WriteLine($"{mathsBook.Grades.Count} Added to {mathsBook.Name} grades book");
+                Console.WriteLine($"{book.Grades.Count} Added to {book.Name} grades book");
                 Console.WriteLine("Please enter a new single grade, or single letter grade, or enter \"q\" to exit and compute the statistics.");
                 string input = Console.ReadLine();
 
-                if(input.ToUpper() == "Q") done = true;
-                else mathsBook.AddGrade(input);
+                if (input.ToUpper() == "Q") done = true;
+                else book.AddGrade(input);
             }
-            
 
-            var stats = mathsBook.GetStatistics();
-
-            Console.WriteLine($"Average grade for {mathsBook.Name} is = {stats.Average:N2}");
-            Console.WriteLine($"Highest grade in {mathsBook.Name} is = {stats.High:N2}");
-            Console.WriteLine($"Lowest grade in {mathsBook.Name} is = {stats.Low:N2}");
-            Console.WriteLine($"The letter grade in {mathsBook.Name} is = {stats.Letter}");
+            return done;
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
